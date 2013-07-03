e03610bf1025dc351da94350a7f41c8a2d1f8cdf
Emma Guy <emmag87@gmail.com>
2013-07-03 13:10:56 +0100

    added async task which currently just retrieves a random number and updates the ui with an unread count on touch and on load

--- src/dev/emmaguy/pocketwidget/PocketWidgetProvider.java
+++ src/dev/emmaguy/pocketwidget/PocketWidgetProvider.java
@@ -7,44 +7,38 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
-public class PocketWidgetProvider extends AppWidgetProvider
-{
+public class PocketWidgetProvider extends AppWidgetProvider {
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
-    {   	
-        for(int appWidgetId : appWidgetIds)
-        {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	for (int appWidgetId : appWidgetIds) {
+	    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 
-            Intent clickIntent = new Intent(context, PocketWidgetProvider.class);
-            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+	    Intent clickIntent = new Intent(context, PocketWidgetProvider.class);
+	    clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, clickIntent, 0);
-            views.setOnClickPendingIntent(R.id.widget_imageview, pendingIntent);
+	    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, clickIntent, 0);
+	    views.setOnClickPendingIntent(R.id.widget_imageview, pendingIntent);
 
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+	    appWidgetManager.updateAppWidget(appWidgetId, views);
+	    this.onReceive(context, clickIntent);
+	}
     }
 
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
-        if (intent.getAction() == null)
-        {
-            Bundle extras = intent.getExtras();
-            if(extras != null)
-            {
-//                int widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
-//                RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+    public void onReceive(Context context, Intent intent) {
+	if (intent.getAction() == null) {
+	    Bundle extras = intent.getExtras();
+	    if (extras != null) {
+		int widgetId = extras
+			.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 
-        	Toast.makeText(context, "onreceive", Toast.LENGTH_SHORT).show();
-            }
-        }
-        else
-        {
-            super.onReceive(context, intent);
-        }
+		new RetrieveUnreadPocketItemsAsyncTask(remoteViews, widgetId, AppWidgetManager.getInstance(context))
+			.execute();
+	    }
+	}
+	
+	super.onReceive(context, intent);
     }
 }
---
+++ src/dev/emmaguy/pocketwidget/RetrieveUnreadPocketItemsAsyncTask.java
@@ -0,0 +1,34 @@
+package dev.emmaguy.pocketwidget;
+
+import java.util.Random;
+
+import android.appwidget.AppWidgetManager;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.RemoteViews;
+
+public class RetrieveUnreadPocketItemsAsyncTask extends AsyncTask<Void, Void, Integer> {
+
+    private final AppWidgetManager appWidgetManager;
+    private final RemoteViews remoteViews;
+    private final int widgetId;
+
+    public RetrieveUnreadPocketItemsAsyncTask(RemoteViews remoteViews, int widgetId, AppWidgetManager appWidgetManager) {
+	this.remoteViews = remoteViews;
+	this.widgetId = widgetId;
+	this.appWidgetManager = appWidgetManager;
+    }
+
+    @Override
+    protected Integer doInBackground(Void... arg0) {
+
+	return new Random().nextInt(1000);
+    }
+
+    @Override
+    protected void onPostExecute(Integer unreadCount) {
+	this.remoteViews.setViewVisibility(R.id.unread_count_textview, View.VISIBLE);
+	this.remoteViews.setTextViewText(R.id.unread_count_textview, unreadCount.toString());
+	this.appWidgetManager.updateAppWidget(widgetId, remoteViews);
+    }
+}
